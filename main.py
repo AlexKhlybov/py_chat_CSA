94bcd1ef98608cd9bc3f21d6835d796fe62ef622
AlexKhlybov <aghlybov@gmail.com>
2021-07-10 23:59:42 +0300

    Server OOP

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import argparse
 
 from settings.variables import DEFAULT_IP_ADDRESS, DEFAULT_PORT, DEFAULT_SERVER
-from src import client, server
+from src import client
+from src.server import Server
 
 
-def mainParser():
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--type", type=str, default=DEFAULT_SERVER)
     parser.add_argument("-p", "--port", type=int, default=DEFAULT_PORT)
     parser.add_argument("-a", "--addr", type=str, default=DEFAULT_IP_ADDRESS)
+    parser.add_argument("-n", "--name", type=str, default=None)
     return parser
 
 
 def start():
-    parser = mainParser()
+    parser = parse_args()
     namespace = parser.parse_args()
     return namespace
 
 
 if __name__ == "__main__":
-
-    what_run = start()
-    if what_run.type == "server":
-        server.main(what_run)
-    elif what_run.type == "client":
-        client.main(what_run)
+    ns = start()
+    if ns.type == "server":
+        # server.main(what_run)
+        server = Server(ns.addr, ns.port)
+        server.main()
+    elif ns.type == "client":
+        client.main(ns)
